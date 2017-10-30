90e1892f3f7198c016f22c931b26ca1ba9e01bb6
Kittipob <kittipob.pa@ku.th>
2017-10-31 04:28:47 +0700

    create human

--- silver.py
+++ silver.py
@@ -1,18 +1,20 @@
 import arcade
-SCREEN_WIDTH = 600
+SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 600
 
 class SilverBulletGameWindow(arcade.Window):
 	def __init__(self, width, height):
 		super().__init__(width, height)
- 
 		arcade.set_background_color(arcade.color.WHITE)
+		self.human_sprite = arcade.Sprite('images/human.png')
+		self.human_sprite.set_position(100, 100)
  
 		
  
  
 	def on_draw(self):
 		arcade.start_render()
+		self.human_sprite.draw()
 		
  
 	
