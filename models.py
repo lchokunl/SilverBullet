5318c5301eedbfc11284996aa7f8950c561b9a00
Kittipob <kittipob.pa@ku.th>
2017-11-02 04:12:35 +0700

    update human1

--- models.py
+++ models.py
@@ -19,7 +19,7 @@ class ModelSprite(arcade.Sprite):
 		self.sync_with_model()
 		super().draw()
 
-class Human:
+class Human1:
 	
 	def __init__(self, world, x, y):
 		self.world = world
@@ -47,9 +47,9 @@ class Human:
 			
 class Bullet():
 	
-	def __init__(self, world, human, x, y):
+	def __init__(self, world, human1, x, y):
 		self.world = world
-		self.human = human
+		self.human1 = human1
 		self.x = x
 		self.y = y
 		self.bullet_list = None
@@ -66,28 +66,31 @@ class World:
 	def __init__(self, width, height):
 		self.width = width
 		self.height = height
-		self.score = 5
+		self.health = 5
  
-		self.human = Human(self, 100, 100)
+		self.human1 = Human1(self, 100, 100)
 		self.bullet = Bullet(self,self,100, 100)
 		self.bullet_list = self.bullet.bullet_list
  
 	def update(self, delta):
-		self.human.update(delta)
+		self.human1.update(delta)
 		self.bullet_list.update()
+		
+		if self.human1.hit(self.bullet, 30):
+            self.health -= 1
 		
 	def on_key_press(self, key, key_modifiers):
 		if key == arcade.key.W:
-			self.human.direction = DIR_UP
+			self.human1.direction = DIR_UP
 		
 		if key == arcade.key.D:
-			self.human.direction = DIR_RIGHT
+			self.human1.direction = DIR_RIGHT
 		
 		if key == arcade.key.A:
-			self.human.direction = DIR_LEFT
+			self.human1.direction = DIR_LEFT
 			
 		if key == arcade.key.S:
-			self.human.direction = DIR_DOWN
+			self.human1.direction = DIR_DOWN
 			
 		if key == arcade.key.SPACE:
 			self.bullet_list = ModelSprite('images/bullet.png',model=self.world.bullet)
