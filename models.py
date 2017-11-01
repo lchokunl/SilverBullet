0b56e32c973af438d5035b18465e4d77e96533ae
Kittipob <kittipob.pa@ku.th>
2017-11-02 03:43:10 +0700

    add def hit

--- models.py
+++ models.py
@@ -5,6 +5,20 @@ DIR_RIGHT = 2
 DIR_LEFT = 3
 DIR_DOWN = 4
 
+class ModelSprite(arcade.Sprite):
+	def __init__(self, *args, **kwargs):
+		self.model = kwargs.pop('model', None)
+ 
+		super().__init__(*args, **kwargs)
+ 
+	def sync_with_model(self):
+		if self.model:
+			self.set_position(self.model.x, self.model.y)
+ 
+	def draw(self):
+		self.sync_with_model()
+		super().draw()
+
 class Human:
 	
 	def __init__(self, world, x, y):
@@ -27,17 +41,40 @@ class Human:
 		
 		if self.direction == DIR_DOWN and self.y > 50:
 			self.y -= 5
-		
-class World:
-	def __init__(self, width, height):
-		self.width = width
-		self.height = height
- 
-		self.human = Human(self, 100, 100)
+			
+	def hit(self, other, hit_size):
+		return (abs(self.x - other.x) <= hit_size) and (abs(self.y - other.y) <= hit_size)
+			
+class Bullet():
+	
+	def __init__(self, world, human, x, y):
+		self.world = world
+		self.human = human
+		self.x = x
+		self.y = y
+		self.bullet_list = None
+		self.bullet_list = arcade.SpriteList()
+	
  
  
 	def update(self, delta):
+		self.x += 5
+		
+		
+class World:
+
+	def __init__(self, width, height):
+		self.width = width
+		self.height = height
+		self.score = 0
+ 
+		self.human = Human(self, 100, 100)
+		self.bullet = Bullet(self,self,100, 100)
+		self.bullet_list = self.bullet.bullet_list
+ 
+	def update(self, delta):
 		self.human.update(delta)
+		self.bullet_list.update()
 		
 	def on_key_press(self, key, key_modifiers):
 		if key == arcade.key.W:
@@ -51,3 +88,7 @@ class World:
 			
 		if key == arcade.key.S:
 			self.human.direction = DIR_DOWN
+			
+		if key == arcade.key.SPACE:
+			self.bullet_list = ModelSprite('images/bullet.png',model=self.world.bullet)
+			self.bullet_list.append(self,bullet)
